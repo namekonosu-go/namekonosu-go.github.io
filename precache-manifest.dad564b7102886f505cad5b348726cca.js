3d7c6ff65616e90ad0881b7a37c9d99df65f3725
namekonosu-go <46099101+namekonosu-go@users.noreply.github.com>
2018-12-24 21:03:29 +0800

    publish v1.0.1

---
+++ precache-manifest.dad564b7102886f505cad5b348726cca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0bb3f24c7691ad3a9333",
+    "url": "/static/js/main.0bb3f24c.chunk.js"
+  },
+  {
+    "revision": "99cad2d4cfaa005f630e",
+    "url": "/static/js/1.99cad2d4.chunk.js"
+  },
+  {
+    "revision": "0bb3f24c7691ad3a9333",
+    "url": "/static/css/main.a8f88cbd.chunk.css"
+  },
+  {
+    "revision": "426115a94ca9701dfb431af139bdd090",
+    "url": "/index.html"
+  }
+];
