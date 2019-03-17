6ef1e25e7c3d612c9c86d5615f67a8acfac8e88d
namekonosu-go <46099101+namekonosu-go@users.noreply.github.com>
2019-03-17 17:12:08 +0800

    publish v1.0.4

---
+++ precache-manifest.0fc17b21a331c9906416ead8993d3ecd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "02f9f5079b46abcba049",
+    "url": "/static/js/main.02f9f507.chunk.js"
+  },
+  {
+    "revision": "99cad2d4cfaa005f630e",
+    "url": "/static/js/1.99cad2d4.chunk.js"
+  },
+  {
+    "revision": "02f9f5079b46abcba049",
+    "url": "/static/css/main.d5f21c49.chunk.css"
+  },
+  {
+    "revision": "ecb2167026d818fe1240069331886a1e",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nameko/precache-manifest.5f728d9c0bc271fd2fa273b39997d07b.js"
+  "/precache-manifest.0fc17b21a331c9906416ead8993d3ecd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/nameko/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
